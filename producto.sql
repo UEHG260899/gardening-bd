e65a341716bf2b44f725735f58379b888dcedb2f
Uriel <urielenrique99@gmail.com>
2021-06-02 08:50:22 -0500

    cambio : nvchar a varchar

--- producto.sql
+++ producto.sql
@@ -1,15 +1,14 @@
 CREATE TABLE [dbo].[Producto] (
-    [id_producto]          INT          IDENTITY (1, 1) NOT NULL,
-    [id_subcategoria]      INT          NOT NULL,
-    [nombre_producto]      NCHAR (100)  NULL,
-    [descripcion_producto] NCHAR (100)  NULL,
-    [precio_producto]      DECIMAL (18) NULL,
-    [ultima_actualizacion] DATE         NULL,
-    [imagen_producto]      NCHAR (50)   NULL,
-    [cantidad_existencia]  INT          NULL,
-    [stock]                INT          NULL,
-    [descuento]            INT          NULL,
+    [id_producto]          INT           IDENTITY (1, 1) NOT NULL,
+    [id_subcategoria]      INT           NOT NULL,
+    [nombre_producto]      VARCHAR (100) NULL,
+    [descripcion_producto] VARCHAR (100)   NULL,
+    [precio_producto]      DECIMAL (18)  NULL,
+    [ultima_actualizacion] DATE          NULL,
+    [imagen_producto]      VARCHAR (50)    NULL,
+    [cantidad_existencia]  INT           NULL,
+    [stock]                INT           NULL,
+    [descuento]            INT           NULL,
     PRIMARY KEY CLUSTERED ([id_producto] ASC),
     FOREIGN KEY ([id_subcategoria]) REFERENCES [dbo].[Subcategoria] ([id_subcategoria])
 );
-
--- sub-categoria.sql
+++ sub-categoria.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[Subcategoria] (
-    [id_subcategoria]          INT         IDENTITY (1, 1) NOT NULL,
-    [id_categoria]             INT         NOT NULL,
-    [nombre_subcategoria]      NCHAR (50)  NULL,
-    [descripcion_subcategoria] NCHAR (100) NULL,
+    [id_subcategoria]          INT           IDENTITY (1, 1) NOT NULL,
+    [id_categoria]             INT           NOT NULL,
+    [nombre_subcategoria]      VARCHAR (100) NULL,
+    [descripcion_subcategoria] VARCHAR (100)   NULL,
     PRIMARY KEY CLUSTERED ([id_subcategoria] ASC),
     FOREIGN KEY ([id_categoria]) REFERENCES [dbo].[Categoria] ([id_categoria])
 );
