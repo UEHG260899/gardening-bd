b6dcb2207afd7193da6a59473d35e94bd7f179cc
Uriel <urielenrique99@gmail.com>
2021-06-02 17:47:11 -0500

    caambio : nchar a varchar

--- pago.sql
+++ pago.sql
@@ -1,9 +1,8 @@
 CREATE TABLE [dbo].[Pago] (
     [id_pago]      INT         IDENTITY (1, 1) NOT NULL,
     [id_tipo_pago] INT         NULL,
-    [estatus_pago] NCHAR (10)  NULL,
-    [detalle_pago] NCHAR (100) NULL,
+    [estatus_pago] VARCHAR(10)  NULL,
+    [detalle_pago] VARCHAR(100) NULL,
     PRIMARY KEY CLUSTERED ([id_pago] ASC),
     FOREIGN KEY ([id_tipo_pago]) REFERENCES [dbo].[Tipo_Pago] ([id_tipo_pago])
 );
-
--- paqueteria.sql
+++ paqueteria.sql
@@ -1,12 +1,12 @@
 CREATE TABLE [dbo].[Paqueteria] (
     [id_paqueteria]        INT         IDENTITY (1, 1) NOT NULL,
-    [nombre_paqueteria]    NCHAR (50)  NOT NULL,
+    [nombre_paqueteria]    VARCHAR(50)  NOT NULL,
     [rfc_paqueteria]       NCHAR (10)  NOT NULL,
-    [telefono_paqueteria]  NCHAR (10)  NOT NULL,
-    [web_paqueteria]       NCHAR (50)  NOT NULL,
-    [direccion_paqueteria] NCHAR (20)  NOT NULL,
-    [contacto_paqueteria]  NCHAR (100) NOT NULL,
-    [tel_contacto]         NCHAR (10)  NOT NULL,
+    [telefono_paqueteria]  BIGINT  NOT NULL,
+    [web_paqueteria]       VARCHAR(100)  NOT NULL,
+    [direccion_paqueteria] VARCHAR(MAX)  NOT NULL,
+    [contacto_paqueteria]  VARCHAR(500) NOT NULL,
+    [tel_contacto]         BIGINT  NOT NULL,
     PRIMARY KEY CLUSTERED ([id_paqueteria] ASC)
 );
 
